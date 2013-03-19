a9b936019c59dd6174227d8be1cbf685c9e5ddd2
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-03-19 23:46:55 +0100

    Cleaner startup server-side

--- server/00_server.js
+++ server/00_server.js
@@ -1,23 +1,24 @@
-(function(){
-    MAG.require = __meteor_bootstrap__.require;
-    MAG.uploadsFolder = "./uploads";
+MAG.require = __meteor_bootstrap__.require;
+MAG.uploadsFolder = "./uploads";
 
+Meteor.startup(function(){
     var fs = MAG.require('fs');
 
-    if(!fs.existsSync("./" + MAG.imagesFolder)){
-        //Create needed directories
-        _.each([ MAG.imagesFolder , MAG.uploadsFolder ], function(d){
-                fs.mkdirSync(d, 0755);
-        });
+    //Create needed directories
+    _.each([ MAG.imagesFolder , MAG.uploadsFolder ], function(d){
+        if(!fs.existsSync("./" + d)){
+            fs.mkdirSync(d, 0755);
+        }
+    });
 
-        //Symlink MAG.imagesFolder into static_cacheable folder, in dev or prod env
-        //Dev env 
-        if(fs.existsSync(".meteor")){
-            fs.symlinkSync('../../../../images', '.meteor/local/build/static_cacheable/images');
-        }
-        //Prod env
-        else {
-            fs.symlinkSync('../images', 'static_cacheable/images');
-        }
+    //Symlink MAG.imagesFolder into static_cacheable folder, in dev or prod env
+    //Dev env 
+    if(fs.existsSync(".meteor")){
+        fs.symlinkSync('../../../../images', '.meteor/local/build/static_cacheable/images');
     }
-}());
+    //Prod env
+    else if(!fs.existsSync('static_cacheable/images')){
+        fs.symlinkSync('../images', 'static_cacheable/images');
+    }
+
+});
