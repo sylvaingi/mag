e455b130a71f26b579400be419d26fb89561408c
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-05-18 20:58:44 +0200

    Integration of styles, phase one

--- client/views/galleries/galleryList.js
+++ client/views/galleries/galleryList.js
@@ -9,6 +9,10 @@ Template.galleryThumb.helpers({
         return Meteor.users.findOne({_id: this.userId}).profile.name;
     },
 
+    size: function(){
+        return MAG.Pictures.find({galleryId: this._id}).count();
+    },
+
     sample: function(){
         return MAG.Pictures.findOne({galleryId: this._id}, {sort: {createdAt: 1}});
     }
--- client/views/helpers/picture.js
+++ client/views/helpers/picture.js
@@ -1,9 +1,10 @@
-Handlebars.registerHelper("picture", function(picture, size){
+Handlebars.registerHelper("picture", function(picture, clazz, size){
     var url = MAG.Pictures.getPicturePath(picture, size);
     url = Handlebars._escape(url);
-    
+
     var name = Handlebars._escape(picture.name);
+    clazz =  Handlebars._escape(clazz);
 
     return new Handlebars.SafeString(
-        "<img src='"+url+"' alt='"+name+"' title='"+name+"'></img>");
+        "<img src='"+url+"' class='"+clazz+"' alt='"+name+"' title='"+name+"'></img>");
 });
