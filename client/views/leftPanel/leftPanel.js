ba5dde79d806a5f6722a47016a73020335bbad80
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-05-24 17:23:18 +0200

    Add my gallery button

--- client/views/leftPanel/leftPanel.js
+++ client/views/leftPanel/leftPanel.js
@@ -1,22 +1,5 @@
 Template.leftPanel.helpers({
-    background: function(){
-        return "img/bg1.jpg";
-    },
-    title: function(){
-        var title;
-        var page = Meteor.Router.page();
-
-        if(page === "galleryList"){
-            title = "maude & antoine";
-        }
-        else if (page === "form"){
-            title = "Envoi de photos";
-        }
-        else if(page === "gallery"){
-            title = "Photos de ";
-            title += Session.get("currentGalleryCreator").profile.name;
-        }
-
-        return title;
+    gallery: function(){
+        return MAG.Galleries.findOne({userId: Meteor.userId()});
     }
 });
